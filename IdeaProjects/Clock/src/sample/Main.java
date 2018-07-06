84733cd4ea2eb1bf02ad9073c7256032ad86fea5
Danny998 <38108450+Danny998@users.noreply.github.com>
2018-07-06 15:40:45 +0200

    Add files via upload

--- IdeaProjects/Clock/src/sample/Main.java
+++ IdeaProjects/Clock/src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -17,14 +18,27 @@ import java.util.Date.*;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Date gr = new Date(118, 11,32);
         Pane root = new Pane();;
+        SimpleDateFormat year = new SimpleDateFormat("yyyy");
+        SimpleDateFormat month = new SimpleDateFormat("MM");
+        SimpleDateFormat day = new SimpleDateFormat("dd");
+        SimpleDateFormat hours = new SimpleDateFormat("HH");
+        SimpleDateFormat minutes = new SimpleDateFormat("mm");
+        SimpleDateFormat seconds = new SimpleDateFormat("ss");
         Text text= new Text(150,50,"Czas do końca roku blyat");
         /*text.setText("Czas do końca roku blyat");
         text.setX(400);
         text.setY(100);
         */
+        SimpleDateFormat df = new SimpleDateFormat("EEEE yyyy.MM.dd 'Godzina: ' HH:mm:ss");
+        StackPane pane = new StackPane();
+        System.out.print(df.format(gr));
         Date data = new Date();
-        Text date = new Text(200,250,data.toString());
+        int monthtonextyear =  Integer.parseInt(month.format(data)) - Integer.parseInt(month.format(gr));
+        int daytonextyear =  Integer.parseInt(day.format(data)) - Integer.parseInt(day.format(gr));
+        System.out.print(daytonextyear);
+        Text date = new Text(200,250,df.format(data));
         root.getChildren().addAll(date,text);
         Scene scene = new Scene(root,500,500);
 
