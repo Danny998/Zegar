c938c95caec2f008d5f29403c42cd9ff8a88dc79
Danny998 <38108450+Danny998@users.noreply.github.com>
2018-07-06 18:02:47 +0200

    Add files via upload

---
+++ IdeaProjects/Clock/src/sample/Leapyear.java
@@ -0,0 +1,19 @@
+package sample;
+import java.util.*;
+public class Leapyear extends Main {
+    public static void main(String [] args) {
+        Main acctualyear = new Main();
+        Main nextyear = new Main();
+        int a = acctualyear.getacctualYear();
+        int n = nextyear.getnextYear();
+        if(a%4==0 && a%100!=0 || a%400==0 ){
+
+        }else{}
+
+    }
+    }
+
+
+
+
+
--- IdeaProjects/Clock/src/sample/Main.java
+++ IdeaProjects/Clock/src/sample/Main.java
@@ -1,5 +1,4 @@
 package sample;
-
 import java.text.SimpleDateFormat;
 import java.util.*;
 import javafx.application.Application;
@@ -14,15 +13,33 @@ import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import java.util.Date.*;
 
-
 public class Main extends Application {
+    SimpleDateFormat year = new SimpleDateFormat("yyyy");
+    Date data = new Date();
+    Date gr = new Date(118, 11,32);
+    public int getacctualYear() {
+        int x = Integer.parseInt(year.format(data));
+        return x;
+    }
+    public int getnextYear(){
+        int y = Integer.parseInt(year.format(gr));
+        return y;
+    }
+
+
     @Override
     public void start(Stage primaryStage) {
-        Date gr = new Date(118, 11,32);
-        Pane root = new Pane();;
-        SimpleDateFormat year = new SimpleDateFormat("yyyy");
+
+        final int dayinyear = 365;
+        final int monthinyear = 12;
+        final int mininhours = 60;
+        final int secinmin = 60;
+        int allsecondsleft;
+        //adding data variables
+        Pane root = new Pane();
+        SimpleDateFormat df = new SimpleDateFormat("EEEE yyyy.mm.dd 'Godzina: ' HH:mm:ss");;
         SimpleDateFormat month = new SimpleDateFormat("MM");
-        SimpleDateFormat day = new SimpleDateFormat("dd");
+        SimpleDateFormat day = new SimpleDateFormat("D");
         SimpleDateFormat hours = new SimpleDateFormat("HH");
         SimpleDateFormat minutes = new SimpleDateFormat("mm");
         SimpleDateFormat seconds = new SimpleDateFormat("ss");
@@ -31,13 +48,24 @@ public class Main extends Application {
         text.setX(400);
         text.setY(100);
         */
-        SimpleDateFormat df = new SimpleDateFormat("EEEE yyyy.MM.dd 'Godzina: ' HH:mm:ss");
         StackPane pane = new StackPane();
-        System.out.print(df.format(gr));
-        Date data = new Date();
-        int monthtonextyear =  Integer.parseInt(month.format(data)) - Integer.parseInt(month.format(gr));
-        int daytonextyear =  Integer.parseInt(day.format(data)) - Integer.parseInt(day.format(gr));
-        System.out.print(daytonextyear);
+        int yearinnextsyear = Integer.parseInt(year.format(gr));
+        int monthinthisyear =  Integer.parseInt(month.format(data));int monthinnextyear = Integer.parseInt(month.format(gr));
+        int dayinthisyear =  Integer.parseInt(day.format(data));int daysinnextyear = Integer.parseInt(day.format(gr));
+        int hoursinthisyear = Integer.parseInt(hours.format(data));int hoursinnextyear = Integer.parseInt(hours.format(gr));
+        int minutesinthisyear = Integer.parseInt(minutes.format(data));int minutesinnextyear = Integer.parseInt(minutes.format(gr));
+        int secondsinthisyear = Integer.parseInt(seconds.format(data));int secondsinnextyear = Integer.parseInt(seconds.format(gr));
+        /*
+        if(hourstonextyear >  0 ){
+            hourstonextyear = hourstonextyear * mininhours * secinmin;
+        }
+        if(minutestonextyear > 0 ) {
+            minutestonextyear = minutestonextyear * secinmin;
+        }
+        allsecondsleft = hourstonextyear + minutestonextyear + secondstonextyear;
+        System.out.println("There is "+monthtonextyear+" months "+daytonextyear+" days "+hourstonextyear+" hours "+minutestonextyear+" minutes "+secondstonextyear+" seconds to next year" );
+        */
+        System.out.println();
         Text date = new Text(200,250,df.format(data));
         root.getChildren().addAll(date,text);
         Scene scene = new Scene(root,500,500);
